84636f376b6864ad24e043476302f61143be13c4
Topaon <simon.granier@sfr.fr>
2023-11-05 15:05:48 +0100

    suppression de la partie base de donnees

--- src/main/java/sg/corporation/chopy/entity/Ingredient.java
+++ src/main/java/sg/corporation/chopy/entity/Ingredient.java
@@ -1,24 +1,18 @@
 package sg.corporation.chopy.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Entity
 @Getter@Setter@NoArgsConstructor@ToString
 public class Ingredient {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
-	public Ingredient(String name) {
+	public Ingredient(Long id, String name) {
+		super();
+		this.id = id;
 		this.name = name;
 	}
 }
--- src/main/java/sg/corporation/chopy/entity/Recipe.java
+++ src/main/java/sg/corporation/chopy/entity/Recipe.java
@@ -2,26 +2,16 @@ package sg.corporation.chopy.entity;
 
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Entity
 @Getter@Setter@NoArgsConstructor@ToString
 public class Recipe {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
-	@ManyToMany
 	private List<Ingredient> ingredients;
 	
 	public Recipe(String name) {
