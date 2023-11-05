4be89da1046926e907c70cbdf595dfe835993ee1
Topaon <simon.granier@sfr.fr>
2023-11-05 13:41:47 +0100

    Test Dep 1 : juste un println

--- src/main/java/sg/corporation/chopy/dao/DaoIngredient.java
+++
@@ -1,9 +0,0 @@
-package sg.corporation.chopy.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sg.corporation.chopy.entity.Ingredient;
-
-public interface DaoIngredient extends JpaRepository<Ingredient, Long> {
-
-}
--- src/main/java/sg/corporation/chopy/dao/DaoRecipe.java
+++
@@ -1,9 +0,0 @@
-package sg.corporation.chopy.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sg.corporation.chopy.entity.Recipe;
-
-public interface DaoRecipe extends JpaRepository<Recipe, Long> {
-
-}
